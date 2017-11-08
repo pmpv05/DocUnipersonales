c895e91b20116ed0b7e8829969acbf6cc2e2e2db
Pablo Piñeiro <pmpv05@hotmail.com>
2017-11-08 14:05:16 -0300

    cambio confirmar verificacion

--- DocumentacionUnipersonales/src/docUni/ConfirmarVerifAdm.java
+++ DocumentacionUnipersonales/src/docUni/ConfirmarVerifAdm.java
@@ -5,23 +5,30 @@ import com.dogma.busClass.BusClassException;
 import com.dogma.busClass.object.Entity;
 import com.dogma.busClass.object.User;
 
-public class ConfirmarVerifAdm extends ApiaAbstractClass{
+public class ConfirmarVerifAdm extends ApiaAbstractClass {
 
 	@Override
 	protected void executeClass() throws BusClassException {
 		// TODO Auto-generated method stub
 		Entity currEnt = this.getCurrentEntity();
-		
-		User usuario = this.getUser(currEnt.getAttribute("P5_DOCUNI_USER").getValueAsString());
-		String mailUsuario = usuario.getEmail();
-		
-		String comentarios = currEnt.getAttribute("P5_DOCUNI_COMENTARIOS_ADM").getValueAsString();
-		String docOk = currEnt.getAttribute("P5_DOCUNI_DOCUMENTOS_OK").getValueAsString();
-		
-		if(docOk.compareTo("true") == 0){
-			Helpers.notificarDocumentosOk(this, mailUsuario, comentarios);
-		}else{
-			currEnt.getAttribute("P5_DOCUNI_OTRAVEZ").setValue("true");
+
+		try {
+			User usuario = this.getUser(currEnt.getAttribute("P5_DOCUNI_USER")
+					.getValueAsString());
+			String mailUsuario = usuario.getEmail();
+
+			String comentarios = currEnt.getAttribute(
+					"P5_DOCUNI_COMENTARIOS_ADM").getValueAsString();
+			String docOk = currEnt.getAttribute("P5_DOCUNI_DOCUMENTOS_OK")
+					.getValueAsString();
+
+			if (docOk.compareTo("true") == 0) {
+				Helpers.notificarDocumentosOk(this, mailUsuario, comentarios);
+			} else {
+				currEnt.getAttribute("P5_DOCUNI_OTRAVEZ").setValue("true");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
